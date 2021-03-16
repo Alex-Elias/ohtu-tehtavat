6bfcefb60a5727fc6b26924a189e1ab3095e07e4
Alex-Elias <alex.pohjanpelto@gmail.com>
2021-03-16 19:16:09 +0200

    Add problems 9-12

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -0,0 +1,22 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Viitegeneraattori implements ReferenceGenerator {
+
+    
+    
+    private int seuraava;
+    
+    @Autowired
+    public Viitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    @Override
+    public int uusi(){
+        return seuraava++;
+    }
+}
